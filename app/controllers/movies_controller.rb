9a18b271094df1f85ac2a21a4afc52c80c0409ca
andre <andrecabralpires@gmail.com>
2022-05-26 09:49:40 +0200

    lecture

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,26 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    if params[:search].present?
+      #@movies = Movie.where(title: params[:search][:query])
+      #@movies = Movie.where("title ILIKE ?", "%#{params[:search][:query]}%")
+      #sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+      #@movies = Movie.where(sql_query, query: "%#{params[:search][:query]}%")
+      # sql_query = " \
+      #   movies.title ILIKE :query \
+      #   OR movies.synopsis ILIKE :query \
+      #   OR directors.first_name ILIKE :query \
+      #   OR directors.last_name ILIKE :query \
+      # "
+      # sql_query = " \
+      #   movies.title @@ :query \
+      #   OR movies.synopsis @@ :query \
+      #   OR directors.first_name @@ :query \
+      #   OR directors.last_name @@ :query \
+      # "
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:search][:query]}%")
+      @movies = Movie.global_search(params[:search][:query])
+    else
+       @movies = Movie.all
+    end
   end
 end
